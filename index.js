62afe935a501bf9df2e6c465e3451daac09b7356
Uditsau <udit251013006133@technoindiaeducation.com>
2026-07-26 19:11:19 +0530

    Day 01

--- index.js
+++ index.js
@@ -85,3 +85,80 @@ console.log(typeof abc);
 console.log(typeof xyz);
 console.log(abc==xyz);
 console.log(abc===xyz);
+
+
+
+//
+console.log(`5`!=5);
+console.log(`5`!==5);
+console.log(`5`!=5,obj.name.third);//undefined karon eta dutoi check korbe, first check korbe 5!=5, then obj.name.third check korbe, jeta undefined, so output hobe false
+console.log(`5`!==5,obj.name.third);//undefined karon eta dutoi check korbe na, first check korbe 5!==5,r jehetu setai thik tai secong ta r check korbe na, so output hobe true
+
+
+
+
+
+if(2>3){
+    console.log("wrong  ");
+}else{
+    console.log(`this will print`)
+    if(true){
+        console.log(`will this print?`);
+    }
+}
+
+
+
+
+if(2>3){
+    console.log("wrong  ");
+}else{
+    console.log(`this will print`)
+    const str = ``
+    let confusion = 500 + `abc`;
+    console.log(confusion);
+
+    if(str||confusion){
+        console.log(`will this print?`);
+    }
+}
+
+
+if(2>3){
+    console.log("wrong  ");
+}else{
+    console.log(`this will print`)
+    const str = ``
+    let confusion = 500 * `abc`;
+    console.log(confusion);
+
+    if(str||confusion){
+        console.log(`will this print?`);
+    }
+}
+
+
+
+const marks = {
+    math: 90,
+    science: 80,
+    english: 70,
+    practical: {
+        science: 20
+    }
+}
+
+
+
+
+
+
+let total= marks.math + marks.science + marks.english;
+total += (marks.practical && marks.practical.science)||0
+console.log(`Total marks: ${total}`);
+
+console.log(true==1)
+console.log(5+parseInt(`4a`));
+console.log((0.1+0.2).toFixed(2));// tofixed(2) will round the number to 2 decimal places and return a string representation of the number. In this case, it will return "0.30" as a string.
+const newChar=`Udit`
+console.log(newChar.toLowerCase()==`udit`);
